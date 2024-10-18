5d9af24a2a5cd7459a69c9ae8e9b85777c4502b4
dicky saputra <sap.dicky19@gmail.com>
2024-10-18 14:40:46 +0700

    add files

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,20 @@
+describe("Login Test", () => {
+  it("Success login to dashboard", () => {
+    // Buka halaman login
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.wait(5000);
+
+    // Input username
+    cy.get('input[name="username"]').type("Admin");
+
+    // Input password
+    cy.get('input[name="password"]').type("admin123");
+
+    // Klik tombol login
+    cy.get('button[type="submit"]').click();
+    cy.wait(5000);
+
+    // Verifikasi apakah tulisan "dashboard" muncul di halaman setelah login
+    cy.contains("Dashboard").should("be.visible");
+  });
+});
